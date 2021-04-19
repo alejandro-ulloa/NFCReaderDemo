8ce68298ddae292f9008e520ba0c1ee396f735a4
AlejandroUlloa <alejandro.ulloa@devsu.com>
2021-04-19 09:49:20 -0500

    Initial Commit

---
+++ NFC Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFC Demo
+//
+//  Created by Alejandro Ulloa on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFC Demo/NFC_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFC_DemoApp.swift
+//  NFC Demo
+//
+//  Created by Alejandro Ulloa on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct NFC_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
